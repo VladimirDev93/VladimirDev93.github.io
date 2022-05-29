4730eb03b191eeeb5b8987c3d40027427e156fbb
Vladimir Jovanovic <me@vladio.me>
2022-05-29 14:42:01 +0200

    refactor(Hero): Adds social links

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,14 +1,23 @@
 import styles from "./hero.module.scss";
+import SocialIcons from "./social-icons/SocialIcons";
 
 const Hero = () => (
   <main className={styles.main}>
     <div className={styles.avatar}>
-      <img src="/img/me.webp" alt="Avatar" width={478} height={478} className={styles.avatarImage} />
-      <p>SOCIAL_MEDIA_LINKS</p>
+      <img
+        src="/img/me.webp"
+        alt="Avatar"
+        width={478}
+        height={478}
+        className={styles.avatarImage}
+      />
+      <SocialIcons />
     </div>
     <div className={styles.intro}>
       <p>Hi,</p>
-      <p>I'm <b>Vladimir.</b></p>
+      <p>
+        I'm <b>Vladimir.</b>
+      </p>
       <h1>Software Engineer</h1>
     </div>
   </main>
