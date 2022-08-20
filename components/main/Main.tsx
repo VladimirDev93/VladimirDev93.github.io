974b6355e990bc8658268842b384e3c68ab079d4
Vladimir Jovanovic <me@vladio.me>
2022-08-20 13:55:31 +0200

    fix(Main): Resolves ESLint validation

--- components/main/Main.tsx
+++ components/main/Main.tsx
@@ -3,7 +3,7 @@ import style from './main.module.scss';
 
 const Main = () => (
   <main className={style.main}>
-    <h1>Hi, I'm Vladimir!</h1>
+    <h1>Hi, I&apos;m Vladimir!</h1>
     <p>Software Engineer, nature and cat lover, amateur astronomer.</p>
     <div className={style.social}>
       <SocialIcons />
