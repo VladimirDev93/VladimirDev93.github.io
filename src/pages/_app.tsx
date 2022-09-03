b128f589a80bc3141d88144b92ceabef9e88e0d4
Vladimir Jovanovic <me@vladio.me>
2022-09-03 16:13:47 +0200

    chore(SEO): Adds title, description

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,18 @@
 import '../styles/globals.scss';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Vladimir Jovanovic | Software Engineer</title>
+        <meta name="description" content="Software Engineer, nature and cat lover, amateur astronomer."/>
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
